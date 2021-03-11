1e5a3b4e72dbe8e6aa5552eb371fa0d65fbfbd01
digitsmart <69549818+digitsmart@users.noreply.github.com>
2021-03-11 09:04:58 -0800

    Add files via upload

---
+++ article/js/article.js
@@ -0,0 +1,104 @@
+(function(){
+
+    var popupCenter = function(url, title, width, height){
+        var popupWidth = width || 640;
+        var popupHeight = height || 320;
+        var windowLeft = window.screenLeft || window.screenX;
+        var windowTop = window.screenTop || window.screenY;
+        var windowWidth = window.innerWidth || document.documentElement.clientWidth;
+        var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+        var popupLeft = windowLeft + windowWidth / 2 - popupWidth / 2 ;
+        var popupTop = windowTop + windowHeight / 2 - popupHeight / 2;
+        var popup = window.open(url, title, 'scrollbars=yes, width=' + popupWidth + ', height=' + popupHeight + ', top=' + popupTop + ', left=' + popupLeft);
+        popup.focus();
+        return true;
+    };
+
+    document.querySelector('.share_twitter').addEventListener('click', function(e){
+        e.preventDefault();
+        var url = this.getAttribute('data-url');
+        var shareUrl = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(document.title) +
+            "&via=Grafikart_fr" +
+            "&url=" + encodeURIComponent(url);
+        popupCenter(shareUrl, "Partager sur Twitter");
+    });
+
+    document.querySelector('.share_facebook').addEventListener('click', function(e){
+        e.preventDefault();
+        var url = this.getAttribute('data-url');
+        var shareUrl = "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(url);
+        popupCenter(shareUrl, "Partager sur facebook");
+    });
+
+    document.querySelector('.share_gplus').addEventListener('click', function(e){
+        e.preventDefault();
+        var url = this.getAttribute('data-url');
+        var shareUrl = "https://plus.google.com/share?url=" + encodeURIComponent(url);
+        popupCenter(shareUrl, "Partager sur Google+");
+    });
+
+    document.querySelector('.share_linkedin').addEventListener('click', function(e){
+        e.preventDefault();
+        var url = this.getAttribute('data-url');
+        var shareUrl = "https://www.linkedin.com/shareArticle?url=" + encodeURIComponent(url);
+        popupCenter(shareUrl, "Partager sur Linkedin");
+    });
+
+})();
+
+    // Function to share on whatsapp 
+    function share() { 
+  
+        // Getting user input 
+        var message = $("input[name=message]").val(); 
+
+        // Opening URL 
+        window.open( 
+            "whatsapp://send?text=" + message, 
+
+            // This is what makes it  
+            // open in a new window. 
+            '_blank'  
+        ); 
+    } 
+
+    const onglets = document.querySelectorAll('.onglets');
+const contenu = document.querySelectorAll('.contenu')
+let index = 0;
+
+onglets.forEach(onglet => {
+
+    onglet.addEventListener('click', () => {
+
+        if(onglet.classList.contains('active')){
+            return;
+        } else {
+            onglet.classList.add('active');
+        }
+
+        index = onglet.getAttribute('data-anim');
+        console.log(index);
+        
+        for(i = 0; i < onglets.length; i++) {
+
+            if(onglets[i].getAttribute('data-anim') != index) {
+                onglets[i].classList.remove('active');
+            }
+
+        }
+
+        for(j = 0; j < contenu.length; j++){
+
+            if(contenu[j].getAttribute('data-anim') == index) {
+                contenu[j].classList.add('activeContenu');
+            } else {
+                contenu[j].classList.remove('activeContenu');
+            }
+            
+
+        }
+
+
+    })
+
+})
